cdc4e7e3aa9947f53bd5692af1dbf62b71d78ff6
pudv95 <vivekupadhayay18@gmail.com>
2024-10-26 00:01:09 +0530

    add teri as seperate table

--- src/Components/Partners/Partners.js
+++ src/Components/Partners/Partners.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import './Partner.css';
 
-import dsirLogo from '../../assets/our_partners/aerial-drone-panorama-view-chisinau-moldova-sunrise.jpg';
+import dsirLogo from '../../assets/our_partners/DSIR.png';
 import gridIndiaLogo from '../../assets/our_partners/GRID-INDIA-LOGO-FINAL.jpg';
 import teriLogo from '../../assets/our_partners/main_teri_logo.png';
 
@@ -13,27 +13,36 @@ class Theme extends Component {
                     Our Partners
                 </h1>
 
+                {/* Financial Partner Section */}
                 <div className="financial-partner-container">
-                <h2 className="partner-section-title">Financial Partner</h2>
+                    <h2 className="partner-section-title">Financial Partner</h2>
                     <table className="partners-table">
                         <tbody>
                             <tr>
                                 <td style={{ width: '250px', padding: '10px' }}>
                                     <div
                                         className="partner-image"
-                                        style={{ backgroundImage: `url(${dsirLogo})`, width: '200px', height: '200px', backgroundSize: 'cover' }}
+                                        style={{
+                                            backgroundImage: `url(${dsirLogo})`,
+                                            width: '200px',
+                                            height: '200px',
+                                            backgroundSize: 'cover'
+                                        }}
                                     />
                                 </td>
-                                <td style={{ fontSize: '16px', paddingLeft: '20px' }}> {/* Reduced font size to 16px */}
-                                    Department of Science and Industrial Research (DSIR)
+                                <td style={{ fontSize: '16px', paddingLeft: '20px' }}>
+                                    Department of Science and Industrial Research (DSIR) / Department of Science and Technology
                                 </td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
 
+                {/* Technical Partners Section */}
                 <div className="technical-partner-container">
-                <h2 className="partner-section-title">Technical Partners</h2>
+                    <h2 className="partner-section-title">Technical Partners</h2>
+                    
+                    {/* Table for Grid India */}
                     <table className="partners-table">
                         <tbody>
                             <tr>
@@ -44,25 +53,31 @@ class Theme extends Component {
                                         style={{ width: '200px', height: 'auto' }}
                                     />
                                 </td>
-                                <td style={{ fontSize: '16px', paddingLeft: '20px' }}> {/* Reduced font size to 16px */}
+                                <td style={{ fontSize: '16px', paddingLeft: '20px' }}>
                                     Grid India
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{ width: '250px', padding: '10px' }}>
-                                    <img
-                                        src={teriLogo}
-                                        alt="TERI Logo"
-                                        style={{ width: '200px', height: 'auto' }}
-                                    />
-                                </td>
-                                <td style={{ fontSize: '16px', paddingLeft: '20px' }}> {/* Reduced font size to 16px */}
-                                    TERI (The Energy and Resources Institute)
                                 </td>
                             </tr>
                         </tbody>
                     </table>
-                    <div style={{height:10}}></div>
+
+                    <div className="teri-container">
+                    <h3 className="partner-section-title" >
+                        The Energy and Resources Institute (TERI)
+                    </h3>
+                    <table className="partners-table" style={{ marginTop: '10px' }}>
+                        <tbody>
+                            <tr>
+                                <td style={{ width: '250px', padding: '10px', textAlign: 'start' }}>
+                                    <img
+                                        src={teriLogo}
+                                        alt="TERI Logo"
+                                        style={{ width: '200px', height: 'auto', }}
+                                    />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
                 </div>
             </div>
         );
